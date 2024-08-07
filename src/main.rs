8db2ea073805b405e5ee0cf66a51ab233bd53ac5
pulkitgangwar <pulkitgangwar2016@gmail.com>
2024-08-07 21:34:24 +0530

    added a simple deck based example by using rust

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,42 @@
+use rand::{seq::SliceRandom, thread_rng};
+
+#[derive(Debug)]
+struct Deck {
+    cards: Vec<String>,
+}
+
+impl Deck {
+    fn new() -> Self {
+        let suits = ["Hearts", "Spades", "Diamonds"];
+        let values = ["Ace", "Two", "Three"];
+        let mut cards = vec![];
+
+        for suit in suits {
+            for value in values {
+                let card = format!("{} of {}", suit, value);
+                cards.push(card);
+            }
+        }
+
+        Deck { cards }
+    }
+
+    fn shuffle(&mut self) {
+        let mut rng = thread_rng();
+
+        self.cards.shuffle(&mut rng);
+    }
+
+    fn deal(&mut self, num_cards: usize) -> Vec<String> {
+        self.cards.split_off(self.cards.len() - num_cards)
+    }
+}
 fn main() {
-    println!("Hello, world!");
+    let mut deck = Deck::new();
+
+    deck.shuffle();
+    let cards = deck.deal(4);
+
+    println!("Here's your hand {:#?}", cards);
+    println!("Here's your deck {:#?}", deck);
 }
